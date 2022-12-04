4aa50ba9e318772c05857472e61e7ec87aadf71a
DonggiChae <chaedonggi@gmail.com>
2022-12-05 02:07:00 +0900

    fix Cannot find file

--- frontend/src/components/auth/SignIn.jsx
+++ frontend/src/components/auth/SignIn.jsx
@@ -4,7 +4,8 @@ import { Amplify } from "aws-amplify";
 import { Authenticator } from "@aws-amplify/ui-react";
 import "@aws-amplify/ui-react/styles.css";
 
-import awsExports from "../../aws-exports.js";
+import awsExports from "../../aws-exports";
+
 Amplify.configure(awsExports);
 
 export default function SignIn() {
