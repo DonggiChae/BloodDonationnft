ee1320b04dd1794ae3cce5bf33d45caef115ffaf
DonggiChae <chaedonggi@gmail.com>
2022-12-08 23:53:05 +0900

    recreate backend

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,29 +1,9 @@
 export type AmplifyDependentResourcesAttributes = {
     "api": {
-        "BloodDonationnftcontract": {
+        "blooddonationnftcont": {
             "GraphQLAPIKeyOutput": "string",
             "GraphQLAPIIdOutput": "string",
             "GraphQLAPIEndpointOutput": "string"
-        }
-    },
-    "auth": {
-        "BloodDonationnftcontract": {
-            "IdentityPoolId": "string",
-            "IdentityPoolName": "string",
-            "HostedUIDomain": "string",
-            "OAuthMetadata": "string",
-            "UserPoolId": "string",
-            "UserPoolArn": "string",
-            "UserPoolName": "string",
-            "AppClientIDWeb": "string",
-            "AppClientID": "string",
-            "CreatedSNSRole": "string"
-        }
-    },
-    "storage": {
-        "s3blooddonationnftcontractstoragecfc3428b": {
-            "BucketName": "string",
-            "Region": "string"
         }
     }
 }
