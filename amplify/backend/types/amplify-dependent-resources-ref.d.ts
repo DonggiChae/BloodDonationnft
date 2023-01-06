7ca3781c7f37a52a75152540099582265f8f1e31
donggi chae <donggichae@donggiui-MacBookPro.local>
2023-01-06 13:43:59 +0900

    amplify push

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,20 +1,20 @@
 export type AmplifyDependentResourcesAttributes = {
-  api: {
-    BloodDonationnftcontractV1: {
-      GraphQLAPIEndpointOutput: "string";
-      GraphQLAPIIdOutput: "string";
-      GraphQLAPIKeyOutput: "string";
-    };
-  };
-  auth: {
-    BloodDonationV1: {
-      AppClientID: "string";
-      AppClientIDWeb: "string";
-      IdentityPoolId: "string";
-      IdentityPoolName: "string";
-      UserPoolArn: "string";
-      UserPoolId: "string";
-      UserPoolName: "string";
-    };
-  };
-};
+  "api": {
+    "BloodDonationnftcontractV1": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  },
+  "auth": {
+    "BloodDonationV1": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
