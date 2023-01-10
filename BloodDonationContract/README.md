a5d1f1faf8c49c7895f241b105f2a1c3dd7946ae
donggi chae <69336797+DonggiChae@users.noreply.github.com>
2023-01-10 17:05:35 +0900

    new AWS

--- BloodDonationContract/README.md
+++
@@ -1,7 +0,0 @@
-# 헌혈증 nft contract
-
-```shell
-npx hardhat compile
-npx hardhat run scripts/deploy.ts --network klaytn
-npx hardhat run scripts/deploy.ts --network klaytn_cypress
-```
---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,20 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "blooddonationnft": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  },
+  "auth": {
+    "blooddonationnfte0c05577": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
